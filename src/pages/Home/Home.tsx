fe3a77425f3fe5c6fc604320e3f79cedcd9ed9aa
uros-simeunovic <dixypixy007@gmail.com>
2024-03-17 15:56:51 +0100

    backend calls test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,40 +1,17 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
-import Product from "./components/Product";
-import { Container, Grid } from "@mui/material";
-
-export interface IProduct {
-  description?: string;
-  price?: number;
-  _links: {
-    product: {
-      href: string;
-    };
-  };
-}
+import { useEffect } from "react";
+import { Box } from "@mui/material";
 
 const Home = () => {
-  const [products, setProducts] = useState<Array<IProduct> | null>([]);
-
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(
-        "https://be-vidoje.azurewebsites.net/products"
-      );
-      setProducts(response.data._embedded.products);
+      const response = await axios.get("https://be-objects-main.onrender.com");
+      console.log(response);
     };
     fetchData();
-  }, [JSON.stringify(products)]);
+  }, []);
 
-  return (
-    <Container>
-      <Grid container spacing={3} mt={1}>
-        {products?.map((item: IProduct, index: number) => (
-          <Product key={index} {...item} />
-        ))}
-      </Grid>
-    </Container>
-  );
+  return <Box>dasdasdas</Box>;
 };
 
 export default Home;
